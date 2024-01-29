7149cd4843f6aea46f18ee8515eb14a1ec981024
MirFahad <l1s21bsse0042@ucp.edu.pk>
2024-01-29 12:04:37 +0500

    Funtions Delation Commited

---
+++ HotelRoom.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+public class Customer
+{
+
+ private:
+    int age;
+    string name;
+    string cnic;
+    
+    public:
+    void BookRoom();
+}
+
+public class HotelRoom
+{
+    private:
+    int rooms;
+    string date;
+    string location;
+    Database customerDatabase;
+    
+    public:
+    void ProcessPayment();
+    void SeachRoom();
+    void CheckAvalibility():
+    void CheckRoomDetails();
+    void AssignRooms();
+    void CheckStaff();
+}
+
+int main() 
+{
+    
+    
+  
+}
